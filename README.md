f934156c5639e17658c6f45a7df4fb6f31d2a8d3
carmelom <carmelo.mordini@unitn.it>
2020-08-17 16:45:51 +0200

    Wrap in <span> element for use of Emoji font

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ YAMEE: Yet Another Markdown Emoji Extension
 
 Inspired by <https://bytefish.de/blog/markdown_emoji_extension/>, but powered by the [emoji](https://pypi.org/project/emoji/) Python package: no need for a .png folder / a json font file, and supports aliases.
 
+Another stolen idea form <https://github.com/kernc/mdx_unimoji>: wrap the unicode charachter in a suitable html element to use an Emoji font.
+
 ## Install
 Install (I'm not on PyPI yet)
 
@@ -16,7 +18,7 @@ Configure Pelican
 from yamee import EmojiExtension
 
 MARKDOWN = {
-    'extensions': [EmojiExtension.get_extension()],
+    'extensions': [EmojiExtension.get_extension(span_class='emoji')],
     'extension_configs': {
         'markdown.extensions.codehilite': {'css_class': 'highlight'},
         'markdown.extensions.toc': {},
@@ -26,6 +28,19 @@ MARKDOWN = {
     'output_format': 'html5',
 }
 ```
+
+Use the `span_class` in your css like:
+```css
+.emoji {
+    font-family: "Apple Color Emoji", "Segoe UI Emoji",
+                 "Noto Color Emoji", EmojiSymbols, "DejaVu Sans", Symbola;
+}
+```
+
+Make sure your font files are locally / globally installed and available. Some (Arch-oriented) source:
+
+- Noto Color Emoji: <https://www.archlinux.org/packages/extra/any/noto-fonts-emoji/>
+- Apple Color Emoji: <https://aur.archlinux.org/packages/ttf-apple-emoji/>
 
 ## Usage
 
--- yamee/yamee.py
+++ yamee/yamee.py
@@ -3,13 +3,10 @@
 #
 # Created: 08-2020 - Carmelo Mordini <carmelo> <carmelo.mordini@unitn.it>
 
-"""Module docstring
-
-"""
-
 import emoji
 from markdown.extensions import Extension
 from markdown.inlinepatterns import Pattern
+from markdown.util import etree
 
 EMOJI_RE = r'(::)(.*?)::'
 
@@ -17,23 +14,40 @@ EMOJI_RE = r'(::)(.*?)::'
 class EmojiExtension(Extension):
 
     def __init__(self, **kwargs):
-        self.config = {}
+        self.config = {
+            'span_class': [
+                'emoji',
+                'the class name of the encompassing `<span>` element (default: "emoji")'
+                'if None, no element is created'
+            ]
+        }
         super(EmojiExtension, self).__init__(**kwargs)
 
     def extendMarkdown(self, md, md_globals):
-        pattern = EmojiInlinePattern(EMOJI_RE)
+        pattern = EmojiInlinePattern(EMOJI_RE, self)
         md.inlinePatterns.add('emoji', pattern, '<not_strong')
 
     @staticmethod
-    def get_extension():
-        return EmojiExtension()
+    def get_extension(**kwargs):
+        return EmojiExtension(**kwargs)
 
 
 class EmojiInlinePattern(Pattern):
 
-    def __init__(self, pattern):
+    def __init__(self, pattern, extension):
         super(EmojiInlinePattern, self).__init__(pattern)
+        self.ext = extension
 
     def handleMatch(self, m):
         emoji_key = m.group(3)
-        return emoji.emojize(f":{emoji_key}:", use_aliases=True)
+        # get the Unicode symbol
+        emoticon = emoji.emojize(f":{emoji_key}:", use_aliases=True)
+        span_class = self.ext.getConfig('span_class')
+        if span_class:
+            # Apply style formatting
+            element = etree.Element('span')
+            element.text = emoticon
+            element.set('class', span_class)
+            return element
+        else:
+            return emoticon
